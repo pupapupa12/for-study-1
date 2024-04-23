66b28bf2ab9eebdf8b1e0edf13b2ede780d3c2c3
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-04-24 04:09:45 +0900

    0423_pyo

--- sudy0422_won.js
+++ sudy0422_won.js
@@ -4,54 +4,63 @@ let num = 1;
 
 console.log(num);
 
-const OddNum = (num) => {
-    let res;
-    if(num===0){
-        // res = "0";
-        console.log("0");
-    }else if(num%2===1){
-        //res = "홀수";
-        console.log("홀수");
-    }else {
-            // 2로 나눴을 때 나머지가 0
-                console.log("짝수");
-    }
+const OddNum = (n) => {
+  let res;
+
+  if (n === 1) {
+    console.log("음수는 짝수 홀수 판변 불가능");
+    res = 1;
+  }
+
+  if (n === 1) {
+    console.log("바보");
+    res = 2;
+  }
+
+  if (n === 0) {
+    // res = "0";
+    console.log("0");
+  } else if (n % 2 === 1) {
+    //res = "홀수";
+    console.log("홀수");
     return res;
+  } else {
+    // 2로 나눴을 때 나머지가 0
+    console.log("짝수");
+    return;
+  }
+
+  return res;
 };
 
-OddNum(num);
-
+const result = OddNum(num);
+console.log(result);
 
 const Gugudan = (num) => {
-    for(let i=1; i<10; i++){
-        console.log(`${num} x ${i} = ${num*i}`);
-    }
-
-}
+  for (let i = 1; i < 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+  }
+};
 
 Gugudan(num);
 
 // 함수 작성하면 꼭 함수 실행해줘야됨
 
-
-const a = [1,2,3,4,5];
+const a = [1, 2, 3, 4, 5];
 
 let res = 0;
-for(let i=0; i<a.length; i++){
-    res = res + a[i];
-    // =>같은 의미: res += a[i];
+for (let pyo = 0; pyo < a.length; pyo++) {
+  console.log(a[pyo]);
+  res = res + a[pyo];
+  // =>같은 의미: res += a[i];
 }
 
 console.log(res);
 
 const obj = {
-    name: "이성원",
-    age: 27,
+  name: "이성원",
+  age: 27,
 };
 
-console.log(obj.name)
-console.log(obj.age)
-
-
-
-
+console.log(obj.name);
+console.log(obj.age);
